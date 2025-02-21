85b50dc41ac835ead206b7997b05e310bf7e7206
fordfishman <fordfishman@gmail.com>
2025-02-21 08:21:01 -0500

    mkinit init files

--- src/__init__.py
+++ src/__init__.py
@@ -1,46 +1,43 @@
-from . import app
-from .classes import Bench, Board, Pool, Shop, Unit, util
-
-# import lazy_loader
+import lazy_loader
 
 
-# __getattr__, __dir__, __all__ = lazy_loader.attach(
-#     __name__,
-#     submodules={
-#         'app',
-#         'classes',
-#         'tests',
-#         'utils',
-#     },
-#     submod_attrs={
-#         'app': [
-#             'app',
-#             'external_stylesheets',
-#             'pool',
-#             'server',
-#             'shops',
-#             'update_number_of_shops',
-#         ],
-#         'classes': [
-#             'Bench',
-#             'Board',
-#             'Pool',
-#             'Shop',
-#             'TestPool',
-#             'TestShop',
-#             'TestUnit',
-#             'Unit',
-#             'load_shop_odds',
-#             'load_units',
-#             'number_shops',
-#             'test',
-#             'util',
-#         ],
-#     },
-# )
+__getattr__, __dir__, __all__ = lazy_loader.attach(
+    __name__,
+    submodules={
+        'app',
+        'classes',
+        'tests',
+        'utils',
+    },
+    submod_attrs={
+        'app': [
+            'app',
+            'external_stylesheets',
+            'pool',
+            'server',
+            'shops',
+            'update_number_of_shops',
+        ],
+        'classes': [
+            'Bench',
+            'Board',
+            'Pool',
+            'Shop',
+            'TestPool',
+            'TestShop',
+            'TestUnit',
+            'Unit',
+            'load_shop_odds',
+            'load_units',
+            'number_shops',
+            'test',
+            'util',
+        ],
+    },
+)
 
-# __all__ = ['Bench', 'Board', 'Pool', 'Shop', 'TestPool', 'TestShop',
-#            'TestUnit', 'Unit', 'app', 'classes', 'external_stylesheets',
-#            'load_shop_odds', 'load_units', 'number_shops', 'pool', 'server',
-#            'shops', 'test', 'tests', 'update_number_of_shops', 'util', 'utils']
+__all__ = ['Bench', 'Board', 'Pool', 'Shop', 'TestPool', 'TestShop',
+           'TestUnit', 'Unit', 'app', 'classes', 'external_stylesheets',
+           'load_shop_odds', 'load_units', 'number_shops', 'pool', 'server',
+           'shops', 'test', 'tests', 'update_number_of_shops', 'util', 'utils']
 
--- src/classes/__init__.py
+++ src/classes/__init__.py
@@ -1,8 +1,41 @@
-from .Bench import *
-from .Board import *
-from .Pool import *
-from .Shop import *
-from .Unit import *
-from .test import *
-from .util import *
+import lazy_loader
+
+
+__getattr__, __dir__, __all__ = lazy_loader.attach(
+    __name__,
+    submodules={
+        'Bench',
+        'Board',
+        'Pool',
+        'Shop',
+        'Unit',
+        'test',
+        'util',
+    },
+    submod_attrs={
+        'Pool': [
+            'Pool',
+        ],
+        'Shop': [
+            'Shop',
+        ],
+        'Unit': [
+            'Unit',
+        ],
+        'test': [
+            'TestPool',
+            'TestShop',
+            'TestUnit',
+        ],
+        'util': [
+            'load_shop_odds',
+            'load_units',
+            'number_shops',
+        ],
+    },
+)
+
+__all__ = ['Bench', 'Board', 'Pool', 'Shop', 'TestPool', 'TestShop',
+           'TestUnit', 'Unit', 'load_shop_odds', 'load_units', 'number_shops',
+           'test', 'util']
 
